6320d28f94f85df879300abb2ea32731bdae97f7
Youssef0095 <jojoaba94@gmail.com>
2021-03-12 11:49:49 +0100

    Updated

--- Models/DhtMeasurement.cs
+++ Models/DhtMeasurement.cs
@@ -11,14 +11,24 @@ namespace AzureFunction.Models
 
         }
 
-        //public DhtMeasurement(string deviceId, DateTime created, decimal temperature, decimal humidity)
-        //{
-        //    DeviceId = deviceId;
-        //    Created = created;
-        //    Temperature = temperature;
-        //    Humidity = humidity;
+        public DhtMeasurement(string deviceId, decimal temperature, decimal humidity, long epochTime)
+        {
+            DeviceId = deviceId;
+            Temperature = temperature;
+            Humidity = humidity;
+            EpochTime = epochTime;
 
-        //}
+        }
+
+        public DhtMeasurement(long id, string deviceId, DateTime created, decimal temperature, decimal humidity)
+        {
+            Id = id;
+            DeviceId = deviceId;
+            Created = created;
+            Temperature = temperature;
+            Humidity = humidity;
+
+        }
 
         public long Id { get; set; }
         public string DeviceId { get; set; }
@@ -27,10 +37,14 @@ namespace AzureFunction.Models
 
         public DateTime Created { get; set; }
         public long EpochTime { get; set; }
+
+        public void ConverEpochTime()
+        {
+            DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(EpochTime);
+            Created = dateTimeOffset.UtcDateTime;
+        }
+
     }
-
-
-
 }
 
 
